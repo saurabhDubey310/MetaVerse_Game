e8e83853ba6c5e1c4cdabd61227fc030d7e9e695
saurabh dubey <dubeysaurabh310@gmail.com>
2024-12-02 20:16:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Hero from './components/Hero';
+import './index.css';
+import About from './components/about';
+import Navbar from './components/Navbar';
+import Features from './components/Features';
+import Story from './components/Story';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+
+const App = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden '>
+      <Navbar />
+      <Hero />
+      <About/>
+      <Features/>
+      <Story/>
+      <Contact/>
+      <Footer/>
+      </main>
+  );
+}
+
+export default App
